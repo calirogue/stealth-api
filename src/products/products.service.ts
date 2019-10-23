9847980e5835de0f7c937135532b6f0286d50adb
Matt Bell <mattbellantoni3@gmail.com>
2019-10-23 06:41:35 +0300

    setting up boiler

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from "@nestjs/common";
 
+import { Product } from './product.model';
 @Injectable()
 export class ProductsService {
-    
+    products: Product[] = [];
+
+    insertProduct(title: string, desc: string, price: number) {
+        const newProduct = new Product(new DataCue().toString(), title, desc, price)
+        this.products.push(newProduct);
+    }
 }
